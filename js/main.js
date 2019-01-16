73da2f48d96ab42ce4477d27f030f07d64ef61aa
Paul Hamill <paul.hamill@yahoo.com>
2019-01-16 14:14:50 -0700

    JS data query using JSONP

--- js/main.js
+++ js/main.js
@@ -5,9 +5,14 @@ $("document").ready(function() {
   $.ajax({
     type: "GET",
     url: dataUrl,
-    dataType: "json",
+    dataType: "jsonp",
     success: function(data) {
       console.log(data);
+    },
+    error: function(jqXHR, status, err) {
+      console.log("Error getting data");
+      console.log(status);
+      console.log(err);
     }
   });
 });
